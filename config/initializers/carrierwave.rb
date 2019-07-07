1a495c92f42bf95f4f8ff07c2b83ec559355909d
mizuno <inc.sinka@gmail.com>
2019-07-08 01:24:53 +0900

    lesson12 no10 ver4

--- config/initializers/carrierwave.rb
+++ config/initializers/carrierwave.rb
@@ -6,11 +6,12 @@ CarrierWave.configure do |config|
   config.storage = :fog
   config.fog_provider = 'fog/aws'
   config.fog_credentials = {
-    :provider              => 'AWS',
-    :region                => ENV['S3_REGION'],
-    :aws_access_key_id     => ENV['S3_ACCESS_KEY'],
-    :aws_secret_access_key => ENV['S3_SECRET_KEY']
+    provider: 'AWS',
+    aws_access_key_id: Rails.application.secrets.aws_access_key_id,
+    aws_secrets_access_key: Rails.application.secrets.aws_secret_access_key,
+    region: 'ap-northeast-1'
   }
-  config.fog_directory     =  ENV['S3_BUCKET']
+
+  config.fog_directory = 'sinka'
   config.asset_host = 'https://s3-ap-northeast-1.amazonaws.com/sinka'
 end
